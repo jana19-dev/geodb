c50a17e70b02a84a55a0acd8c75a4d4898ba3796
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2022-12-09 18:43:21 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import DotPattern from '$lib/components/common/DotPattern.svelte'
+</script>
+
+<DotPattern />
+
+<div class="grid h-full place-items-center">
+  <div class="flex flex-col items-center justify-center gap-4">
+    <h1 class="z-10 text-5xl font-extrabold leading-none tracking-tight text-white">
+      <span class="tracking-wide text-blue-500">GeoDB</span>
+    </h1>
+    <img src="/favicon.png" alt="logo" class="h-32 w-32 animate-spin-slow" />
+  </div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      },
+      animation: {
+        'spin-slow': 'spin 20s linear infinite'
+      }
+    }
+  },
+
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
+}
+
+module.exports = config
